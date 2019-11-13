b9ff9b45b379156f780112807c15c29af6741ef4
Ma Lourdes Santos <marialobsan@gmail.com>
2019-11-13 16:38:23 +0800

    initial commit

---
+++ IAM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAM RICH
+//
+//  Created by Anne Saints on 13/11/2019.
+//  Copyright © 2019 Anne Saints. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
